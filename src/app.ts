2dada066334d247f89361c51fba816b266965796
Charles <codelyfewithcharlie@gmail.com>
2020-09-23 16:42:44 -0400

    practice

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,24 @@
 const userName = 'Charlie';
+
+// const add = (n1: number, n2: number = 13) => n1 + n1;
+
+// console.log(add(5, 78));
+
+const hobbies = ['Sports', 'CooKing'];
+const activeHobbies = ['Hiking'];
+
+activeHobbies.push(...hobbies);
+const person = {
+  name: 'charlie',
+  age: 30,
+};
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+  return numbers.reduce((acc, curr) => {
+    return (acc += curr);
+  }, 0);
+};
+
+console.log(add(1, 23, 4, 2234, 52, 23, 3));
+const [sports, cooking] = hobbies;
