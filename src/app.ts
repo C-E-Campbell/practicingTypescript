e04c662a01264211e48b0d7e04720c6d61240713
Charles <codelyfewithcharlie@gmail.com>
2020-10-03 22:39:24 -0400

    updates

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,9 @@
-class Department {
-  private employees: string[] = [];
+abstract class Department {
+  protected employees: string[] = [];
 
-  constructor(private name: string, private readonly id: number) {}
+  constructor(private name: string, protected readonly id: number) {}
 
-  describe(this: Department) {
-    return `Department: ${this.name}`;
-  }
+  abstract describe(): void;
 
   addEmployee(employee: string) {
     this.employees.push(employee);
@@ -22,6 +20,12 @@ class Department {
   getDepartmentID() {
     return this.id;
   }
+
+  static createEmployee(name: string) {
+    return {
+      name: name,
+    };
+  }
 }
 
 class ITDepartment extends Department {
@@ -30,18 +34,41 @@ class ITDepartment extends Department {
     super('IT', 9);
     this.admins = admins;
   }
+
+  describe() {
+    return this.id;
+  }
 }
 
 class AccountingDepartment extends Department {
+  private lastReport: string;
   constructor(id: number, private reports: string[]) {
     super('Accounting', id);
+    this.lastReport = reports[0];
+  }
+
+  get recentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('No report found.');
+  }
+
+  set recentReport(value: string) {}
+
+  addEmployee(name: string) {
+    this.employees.push(name);
   }
 
   getReports() {
     return this.reports;
   }
-}
 
+  describe() {
+    return this.id;
+  }
+}
+const employee1 = Department.createEmployee('Max');
 const store = new ITDepartment(8, ['Chalrie']);
 store.addEmployee('David');
 
